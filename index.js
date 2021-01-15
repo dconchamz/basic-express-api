0a16eeda817092ec1bc52c7cb630d6c651b72938
Dancko <dancko.concha@azlo.com>
2021-01-15 17:30:17 -0300

    feat: api de prueba

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = express();
 
 const port = process.env.PORT || '3000';
 
+const response = {
+    message: 'Prueba Api Pipeline',
+    dev: 'Dancko Concha Manzor',
+    po: 'Javier caceres'
+}
+
 app.get('/', (req, res) => {
-
-res.send('Your Express API is up and running Test!');
-
+res.send(response);
 });
 
 app.listen(port, () => {
